0d5bee0d0e8c431ecd53c72e815cafc1ed24fd7a
bharper3 <41131130+bharper3@users.noreply.github.com>
2020-04-01 17:15:25 -0500

    Add files via upload

---
+++ ecliptic/data/definitions.js
@@ -0,0 +1,9 @@
+/*
+This file contains question definitions used by a quiz.
+See http://astro.unl.edu/interactives for more information.
+Date published: 2/4/2019, 11:14:32 PM
+Editor version: Experimental, 23 November 2018
+*/
+
+questionDefinitions = {"400999af-18ec-4bf1-aa64-d638b9724516":{"id":"400999af-18ec-4bf1-aa64-d638b9724516","name":"Ecliptic","question":"The ecliptic is shown on a flat map of the celestial sphere. The locations of the sun on several dates/declinations are indicated by letters and represented by icons below. Rank the locations on the ecliptic in chronological order beginning on February 10.","bins":[{"id":"e4e48387-b12c-4b3f-a77f-c995c22b6943","items":[{"id":"4ba03c5c-4817-4991-aceb-184642a5b391","type":"image","value":"4","feedback":"July 21","src":"data/images/7de59af8-4814-48f1-aaa2-ca512d411fe9.png"},{"id":"9956b0c5-34f5-4f7f-9b89-7bba86f61067","type":"image","value":"2","feedback":"April 21","src":"data/images/46187cd2-603e-4164-9b9f-45df169e8392.png"},{"id":"17f03283-3e2c-49ef-997e-ee8e29de6591","type":"image","value":"5","feedback":"December 22","src":"data/images/ccedcba2-c5e6-4ba7-bdd9-aa383c444ad9.png"},{"id":"b6024074-a514-44ef-b583-fa0cb961e22d","type":"image","value":"1","feedback":"March 21","src":"data/images/d5bdb9f6-cc56-457d-aa38-6c96f13a661d.png"},{"id":"6ac5f710-76e0-464a-af63-4a776a9306ec","type":"image","value":"3","feedback":"June 21","src":"data/images/7585578b-e5aa-486f-86b1-39404a90fab6.png"}]}],"numToSelect":"5","background":{"src":"eclipticbackground.html"},"media":{"type":"image","src":"data/images/1d750172-1588-4238-8469-ec75e24b947b.png"}}};
+
